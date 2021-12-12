75f358a052e5dfd185a75b414443b92e7bc40a9e
ajayparihar876 <ajayparihar876@gmail.com>
2021-12-12 20:46:53 +0530

    print

---
+++ print_type.py
@@ -0,0 +1,18 @@
+print("print statement")
+
+a = 'ajay'
+b = 'parihar'
+
+print(f'{a}{b} ')
+print(f'sdfgh7{a} dfgh {b} sdf')
+print(f' my name {a}')
+
+num1 = int(input("Enter the 1 num :-  "))
+print(num1)
+
+
+s = input("enter")
+v = int(s)
+print(v)
+
+print("intiger %d %d" % (num1, v))
